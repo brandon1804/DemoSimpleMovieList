3f0e5bf8704d6ca6132054d9e6f321c8349c76b0
brandon1804 <18044455@rp.edu.sg>
2020-07-16 12:41:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18044455/demosimplemovielist/MainActivity.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.id18044455.demosimplemovielist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvMovie;
+    ArrayList<String> movieList;
+    ArrayAdapter<String> aaMovie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        lvMovie = findViewById(R.id.listViewMovie);
+        movieList = new ArrayList<>();
+        movieList.add("Avengers Infinity War Release Date: 2018.04");
+        movieList.add("Justice League Release Date: 2017.11");
+        aaMovie = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, movieList);
+        lvMovie.setAdapter(aaMovie);
+
+
+
+
+
+
+    }//end of main method
+}//end of class
